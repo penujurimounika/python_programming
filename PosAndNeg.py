f24b9b1025732b8978beb48eefb08d46439257b8
penujurimounika <penujurimounika123@gmail.com>
2024-09-25 10:44:22 +0530

    Add files via upload

---
+++ PosAndNeg.py
@@ -0,0 +1,7 @@
+num=int(input("enter a number"))
+if(num>0):
+    print("Given number is positive")
+elif(num<0):
+    print("Given number is negative")
+elif(num==0):
+    print("Given number is 0")
